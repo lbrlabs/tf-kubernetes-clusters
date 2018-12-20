d95660f242dbd8118d34b54c04d6ba366fde5338
Lee Briggs <lbriggs@apptio.com>
2018-12-20 12:03:34 -0800

    More GKE updates

--- google/main.tf
+++ google/main.tf
@@ -1,7 +1,6 @@
 resource "google_container_cluster" "primary" {
-  name               = "lbr"
+  name               = "${var.name}"
   zone               = "${var.region}-a"
-  initial_node_count = "${var.node_count}"
   project            = "${var.project_id}"
   min_master_version = "${var.version}"
 
@@ -16,6 +15,54 @@ resource "google_container_cluster" "primary" {
       start_time = "03:00"
     }
   }
+
+  lifecycle {
+    ignore_changes = ["node_pool"]
+  }
+
+  node_pool {
+    name = "default-pool"
+  }
+}
+
+resource "google_container_node_pool" "default" {
+  name               = "default"
+  project            = "${var.project_id}"
+  zone               = "${var.region}-a"
+  cluster            = "${google_container_cluster.primary.name}"
+  initial_node_count = "${var.node_count}"
+
+  autoscaling {
+    min_node_count = "${var.node_pool_min_size}"
+    max_node_count = "${var.node_pool_max_size}"
+  }
+
+  management {
+    auto_repair  = "${var.node_auto_repair}"
+    auto_upgrade = "${var.node_auto_upgrade}"
+  }
+
+  lifecycle {
+    ignore_changes = ["initial_node_count"]
+  }
+
+  node_config {
+    machine_type = "${var.node_machine_type}"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  depends_on = ["google_container_cluster.primary"]
+}
+
+resource "null_resource" "kubeconfig" {
+  provisioner "local-exec" {
+    command = "gcloud container clusters get-credentials ${var.name}"
+  }
+
+  depends_on = ["google_container_node_pool.default"]
 }
 
 output "master_version" {
--- google/variables.tf
+++ google/variables.tf
@@ -7,7 +7,29 @@ variable "version" {
 }
 
 variable "node_count" {
-  default = 2
+  default = 1
+}
+
+variable "node_pool_min_size" {
+  default = 0
+}
+
+variable "node_pool_max_size" {
+  default = 5
+}
+
+variable "node_auto_repair" {
+  default = true
+}
+
+variable "node_auto_upgrade" {
+  default = true
+}
+
+variable "node_machine_type" {
+  default = "n1-standard-1"
 }
 
 variable "project_id" {}
+
+variable "name" {}
